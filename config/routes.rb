220be4a31131748cc1008e674e4dd36a7ec9c63d
Dmitry <ddmitiy@mail.ru>
2022-05-09 17:33:10 +0300

    Add devise and setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get '/board', to: 'board#index'
   get '/contact', to: 'board#contact'
   get '/card', to: 'board#card'
+  root 'board#index'
+  get 'persons/profile', as: 'user_root'
 
 end
