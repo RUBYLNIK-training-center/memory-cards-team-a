b68e8de508a638248493ee95e530fdafe1e5d84b
Dmitry <ddmitiy@mail.ru>
2022-05-12 20:16:48 +0300

    Add helpers title

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,12 +2,10 @@ class BoardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
     render text: 'index'
   end
 
   def contact
-
     render text: 'contact'
   end
 end
