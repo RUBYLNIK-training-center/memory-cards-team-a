a976708dadba3654488a2378ff26d78cb9e6d84c
Ubuntu 20.04 <an.shparun@yandex.by>
2022-05-17 13:14:59 +0300

    root welcome#intex as home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'welcome#index', as: 'home'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
