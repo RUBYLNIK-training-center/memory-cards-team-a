05ff9395556b6e17dd28661b6373e2207187987a
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-05-17 11:08:47 +0300

    Add show/hide card feature on the show card page

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -12,3 +12,7 @@ import '../js/bootstrap_js_files.js'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+setTimeout(function(){
+	document.getElementById('message').style.display = 'none';
+}, 5000);
