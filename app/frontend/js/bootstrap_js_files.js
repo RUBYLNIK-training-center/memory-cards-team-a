d131d2b1166acf75c108d4ce487e2e9907407435
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-05-17 10:27:03 +0300

    Add spaces

--- app/frontend/js/bootstrap_js_files.js
+++ app/frontend/js/bootstrap_js_files.js
@@ -8,4 +8,5 @@ import 'bootstrap/js/src/dropdown'
 import 'bootstrap/js/src/scrollspy'  
 // import 'bootstrap/js/src/tab'  
 // import 'bootstrap/js/src/toast'  
-// import 'bootstrap/js/src/tooltip' 
+// import 'bootstrap/js/src/tooltip'
+ 
