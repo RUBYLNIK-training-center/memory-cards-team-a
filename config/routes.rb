e513598f00b9e9db114cf5d47de6515b7ca82851
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-05-18 20:31:01 +0300

    Create route and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :boards
+  resources :boards do
+    resources :cards
+  end
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :cards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_190411) do
+ActiveRecord::Schema.define(version: 2022_05_18_172837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2022_05_17_190411) do
     t.string "answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "board_id"
+    t.index ["board_id"], name: "index_cards_on_board_id"
   end
 
   create_table "users", force: :cascade do |t|
