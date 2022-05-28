44d7ba2918757d13c5b7141b09e388ee65e469ba
Dmitry <ddmitiy@mail.ru>
2022-05-28 12:10:38 +0300

    Remove update password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Method to add
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :cards
     get '/learning', to: 'cards#learn'
   end
-  devise_for :users
+  devise_for :users , controllers: { registrations: 'user/registrations' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
