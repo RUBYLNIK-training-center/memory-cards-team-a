1c724573350331108e87d4ef2add54e02c844ddd
Dmitry <ddmitiy@mail.ru>
2022-05-30 14:31:12 +0300

    Correct auth github and google

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -3,19 +3,20 @@ class OmniauthController < Devise::OmniauthCallbacksController
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     else
-      flash[:error]='There was a problem signing you in through Github. Please register or try signing in later.'
+      flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
   end
+
   def google_oauth2
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
     else
-      flash[:error]='There was a problem signing you in through Google. Please register or try signing in later.'
+      flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   has_many :imports
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [ :github, :google_oauth2]
+         :omniauthable, omniauth_providers: %i[github google_oauth2]
 
-    def self.create_from_provider_data(provider_data)
-      where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
-        user.email = provider_data.info.email
-        user.password = Devise.friendly_token[0, 20]
-      end
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
     end
+  end
+
   # Method to add
   def update_without_current_password(params, *options)
     params.delete(:current_password)
