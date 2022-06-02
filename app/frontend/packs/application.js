0a70c329182a76206ec5a1e80f489e61bfa78803
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-06-02 22:24:06 +0300

    Remove turbolinks

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,13 +4,11 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../js/bootstrap_js_files.js'
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 setTimeout(function(){
