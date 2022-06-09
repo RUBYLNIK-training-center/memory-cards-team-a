9576aa5c876d896b8e72325e4e1625f3bc9a9541
Ubuntu 20.04 <an.shparun@yandex.by>
2022-06-09 19:38:25 +0300

    add avatar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github google_oauth2]
+  has_one_attached :avatar
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
