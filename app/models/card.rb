c120a57939623e5437f8ccf49cc69bf5dc81c77c
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-06-09 09:46:04 +0300

    add export cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,16 @@ class Card < ApplicationRecord
   validates :question, presence: true
   belongs_to :board
   enum confidence_level: %i[undefined very_bad bad medium good perfect]
+
+  def self.to_csv
+    attributes = %w[board_name card_question card_answer]
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |card|
+        csv << [card.id, card.question, card.answer]
+      end
+    end
+  end
 end
