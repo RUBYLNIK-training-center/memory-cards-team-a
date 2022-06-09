a572610e745b5a7e7afecfe0e029d5cb241755a2
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-06-09 12:12:56 +0300

    fix import

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,5 +1,5 @@
 class Import < ApplicationRecord
   belongs_to :user
   has_one_attached :file
-  validates :file, attached: true, content_type: [:csv]
+  validates :file, attached: true, content_type: [:csv], size: { less_than: 5.megabytes , message: 'is too large' }
 end
--- app/services/card_import.rb
+++ app/services/card_import.rb
@@ -8,11 +8,15 @@ class CardImport
     @import.file.open do |tempfile|
       CSV.foreach(tempfile, headers: true) do |row|
         hash = row.to_hash
-        return false if hash.value?(nil)
+        if hash.value?(nil)
+          Board.where(name: hash['board_name'].strip, user_id: @user.id).destroy_all
+          return false
+        end
 
         board = Board.find_or_create_by(name: hash['board_name'].strip, user_id: @user.id)
         board.cards.find_or_create_by(question: hash['card_question'], answer: hash['card_answer'])
       end
     end
+    return true
   end
 end
