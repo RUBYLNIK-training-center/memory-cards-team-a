cf3debe57f878bd6cad79af7338579a58262386f
Dmitry <ddmitiy@mail.ru>
2022-06-14 13:51:53 +0300

    Add rspec Welcome

---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe 'errors', :type => :request do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get 'http://localhost:3000/'
+      expect(response.status).to eq(200)
+    end
+  end
+end
