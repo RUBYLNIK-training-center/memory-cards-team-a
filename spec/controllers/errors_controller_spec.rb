a99a2442e98da52d0bfd6a4c9d624bf121cee350
Dmitry <ddmitiy@mail.ru>
2022-06-18 23:11:41 +0300

    add first spec

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,10 +1,39 @@
 require "rails_helper"
+#
+# RSpec.describe 'errors', type: :controller do
+#   describe "not_found" do
+#     it "GET #most_viewed renders #not_found.html template if html requested" do
+#       get :not_found, :format => 'html'
+#       response.should render_template('not_found')
+#     end
+#   end
+# end
 
-RSpec.describe 'errors', :type => :request do
-  describe "GET index" do
-    it "has a 200 status code" do
-      get 'http://localhost:3000/'
-      expect(response.status).to eq(200)
+RSpec.describe ErrorsController do
+  describe 'not_found' do
+    before do
+      get :not_found, format: :html
+    end
+    it "testing response" do
+    expect(response).to render_template(:not_found)
+    end
+  end
+
+  describe 'unacceptable' do
+    before do
+      get :unacceptable, format: :html
+    end
+    it "testing response" do
+      expect(response).to render_template(:unacceptable)
+    end
+  end
+
+  describe 'internal_error' do
+    before do
+      get :internal_error, format: :html
+    end
+    it "testing response" do
+      expect(response).to render_template(:internal_error)
     end
   end
 end
