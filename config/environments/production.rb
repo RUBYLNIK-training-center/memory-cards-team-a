5790904cb66219dc7aad511e4ea40901930306e4
Ubuntu 20.04 <an.shparun@yandex.by>
2022-06-24 05:34:45 +0300

    add email confirmation, sending an email if you forget your password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,25 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "memory_card_team_a_production"
 
+
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'your-site-url'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost',
+    user_name:            'memory.card.team.a@gmail.com',
+    password:             'qqsrcbbkpgcxnsce',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
