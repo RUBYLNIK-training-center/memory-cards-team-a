aa5e7c96906c5d1ec5f4b230131af13329f10d2b
Ubuntu 20.04 <an.shparun@yandex.by>
2022-06-25 16:56:30 +0300

    adsffdsa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: memory-card-team-a.herokuapp.com }
+  config.action_mailer.default_url_options = { host: 'memory-card-team-a.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
 
 
